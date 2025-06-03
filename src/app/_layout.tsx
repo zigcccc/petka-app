a9122c40fd812d2eeaa4f25c52df4431f697594c
Žiga Krašovec <zkrasovec@gmail.com>
2025-06-03 13:00:26 +0200

    code cleanup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,12 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+    'Inter-Bold': require('@/assets/fonts/inter/Inter-Bold.ttf'),
+    'Inter-Light': require('@/assets/fonts/inter/Inter-Light.ttf'),
+    'Inter-Medium': require('@/assets/fonts/inter/Inter-Medium.ttf'),
+    'Inter-Regular': require('@/assets/fonts/inter/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('@/assets/fonts/inter/Inter-SemiBold.ttf'),
   });
 
   if (!loaded) {
@@ -20,7 +25,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
