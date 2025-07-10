ba4533502c1d0335aa6438935f8f5cc3062eb152
Ziga Krasovec <ziga.krasovec@optifarm.net>
2025-07-10 11:31:09 +0200

    feat: Added @gorhom/bottom-sheet dependency and updated testing setup to include mocks for bottom sheet component

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+import { type PropsWithChildren } from 'react';
+import { Text as MockText } from 'react-native';
 
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
@@ -12,4 +14,14 @@ jest.mock('burnt', () => ({
 jest.mock('expo', () => ({
   ...jest.requireActual('expo'),
   isRunningInExpoGo: jest.fn().mockReturnValue(false),
+}));
+
+jest.mock('@expo/vector-icons', () => ({
+  ...jest.requireActual('@expo/vector-icons'),
+  Octicons: (props: PropsWithChildren) => <MockText {...props} />,
+}));
+
+jest.mock('@gorhom/bottom-sheet', () => ({
+  __esModule: true,
+  ...require('@gorhom/bottom-sheet/mock'),
 }));
