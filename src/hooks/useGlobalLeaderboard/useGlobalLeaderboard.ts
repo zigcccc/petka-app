74f91ef50272c27d76669fa310125c46cbe1bc4a
Ziga Krasovec <ziga.krasovec@optifarm.net>
2025-09-03 19:31:31 +0200

    feat: Removed global leaderboard query hooks

--- src/hooks/useGlobalLeaderboard/useGlobalLeaderboard.ts
+++
@@ -1,9 +0,0 @@
-import { type LeaderboardRange } from '@/convex/leaderboards/models';
-
-import { useGlobalLeaderboardQuery } from '../queries';
-import { useUser } from '../useUser';
-
-export function useGlobalLeaderboard(range: LeaderboardRange) {
-  const { user } = useUser();
-  return useGlobalLeaderboardQuery(user?._id ? { range, userId: user._id } : 'skip');
-}
