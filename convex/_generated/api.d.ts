5060c2dc511b1cb6e505c8ca2eccd91af1b9e557
Ziga Krasovec <ziga.krasovec@optifarm.net>
2025-11-11 07:03:07 +0100

    chore: bumped deps

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -39,14 +39,6 @@ import type {
   FunctionReference,
 } from "convex/server";
 
-/**
- * A utility for referencing Convex functions in your app's API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = api.myModule.myFunction;
- * ```
- */
 declare const fullApi: ApiFromModules<{
   crons: typeof crons;
   "dictionary/models": typeof dictionary_models;
@@ -73,14 +65,30 @@ declare const fullApi: ApiFromModules<{
   "users/models": typeof users_models;
   "users/queries": typeof users_queries;
 }>;
-declare const fullApiWithMounts: typeof fullApi;
 
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export declare const api: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "internal">
 >;
 
