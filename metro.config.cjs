f3b280e638431bf40f8d620409a6482b5f31c54e
Ziga Krasovec <ziga.krasovec@optifarm.net>
2026-05-18 09:49:29 +0200

    chore: Bumped deps

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,6 +1,5 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-// eslint-disable-next-line no-undef
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
